353acc0dfb5622811849c6b4185500e13ed6f61c
nada.ahmed_maxab <nada.elantry4@gmail.com>
2024-01-20 15:52:48 +0200

    Commit new testcases2

---
+++ src/test/java/page/base/Base.java
@@ -0,0 +1,22 @@
+package page.base;
+
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base
+
+{
+    public static WebDriver driver;
+
+
+    @Before
+    public static void launchbrowser()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://magento.softwaretestingboard.com/");
+    }
+}
